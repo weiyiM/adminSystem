cc05acd1a32114eaa0434269810b2d34ccbc8850
maoyaY <1335679005@qq.com>
2018-08-13 17:47:24 +0800

    新建文件夹

---
+++ src/views/index/components/headerTab.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+
+</template>
+
+<script type="text/ecmascript-6">
+    export default {
+        data() {
+            return {
+                name: ''
+            }
+        },
+        components: {},
+        methods: {},
+        created() {
+
+        }
+    }
+</script>
+<style lang="less" rel="stylesheet/less" scoped>
+
+</style>
